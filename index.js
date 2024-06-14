410dd0cf1a2e105f02ebc9e59820af77a4171e7b
sungnam cho <sung6081@gmail.com>
2024-06-14 21:19:01 +0900

    Feat : sung uri 다시

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ const io = socketIo(server, {
 });
 
 //mongoDB연결
-const mongoUri = 'mongodb://223.130.156.104:27017/chat'; // 로컬 MongoDB URI
+const mongoUri = 'mongodb://localhost:27017/chat'; // 로컬 MongoDB URI
 mongoose.connect(mongoUri)
     .then(() => console.log('MongoDB connected'))
     .catch((err) => console.log('MongoDB connection error:', err));
